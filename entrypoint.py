b36c6eee8dcae78d44f81d420ff5435c166bf1c7
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2020-02-06 23:03:50 +0100

    fix tag fetching

--- entrypoint.py
+++ entrypoint.py
@@ -6,7 +6,7 @@ import os
 import json
 import requests
 
-subprocess.run(['git', 'fetch', '--tags'])
+subprocess.run(['git', 'fetch', '--no-tags', '--prune', '--depth=1', 'origin', '+refs/heads/*:refs/remotes/origin/*'])
 results = subprocess.run(['git', 'describe', '--tags'], capture_output=True).stdout.split(maxsplit=1)
 print(results)
 tag = results[0].decode() if results else ''
